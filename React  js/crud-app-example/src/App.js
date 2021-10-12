2e07c3fa763fc702fa2deceff9467d5f8b836f52
Jogini_Suresh <sureshyadav2814@gmail.com>
2021-10-12 17:35:50 +0530

    crud app

---
+++ React  js/crud-app-example/src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import userList from './components/userList';
+
+function App() {
+  return (
+    <div className="App">
+      <userList />
+    </div>
+  );
+}
+
+export default App;
